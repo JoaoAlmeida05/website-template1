63f0b805d6abe52b2754e8c11f19cff8adb69030
Joao Victor Homem Almeida <121456112+JoaoAlmeida05@users.noreply.github.com>
2024-10-17 13:16:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let counter = false
+function mobile_menu(){
+    const home = document.querySelector('.home')
+    if(counter==false){
+        counter = true
+        home.style.visibility = "hidden";
+    }else{
+        counter = false
+        home.style.visibility = "visible";
+    }
+
+    
+
+}
